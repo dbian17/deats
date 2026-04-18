c17dcbbf76aad15b93357225e1fffbe09ae8a455
David Bian <davidto1122@gmail.com>
2026-04-18 03:02:07 -0400

    Add Mantine integration

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import {get_test_data} from './client/flask-client'
 export default async function Home() {
   return (
 
-    <main className="">
         <div> {await get_test_data()}</div> 
-    </main>
   )
 }
