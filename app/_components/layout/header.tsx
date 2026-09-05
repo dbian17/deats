cbbc8a4739c09f71f0fecc8029ed1ba24da73426
David Bian <davidto1122@gmail.com>
2026-09-05 00:25:56 -0400

    Move desktop header tabs to left

--- app/_components/layout/header.tsx
+++ app/_components/layout/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   const router = useRouter();
 
   return (
-    <Group h="100%" bg="customColor.6" justify="flex-end" gap={0}>
+    <Group h="100%" bg="customColor.6" justify="flex-start" gap={0}>
       <Button h="100%" radius={0} onClick={() => router.push("/map")}>
         Map
       </Button>
